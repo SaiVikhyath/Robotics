a10254ee161651e07c66e9a487e0a04b2f036edc
SaiVikhyath <vikhyath456@gmail.com>
2021-08-05 22:12:58 +0530

    Read Pin

---
+++ ReadPin/ReadPin.ino
@@ -0,0 +1,15 @@
+void setup() 
+{
+  // put your setup code here, to run once:
+  pinMode(11, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(11));
+  delay(1000);
+  
+}
